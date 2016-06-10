9483649d21a17426229e1ae301e4579362b621f9
bearachute <sjourand@gmail.com>
2016-06-10 03:46:27 -0500

    various stupid shit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi World!");
+
+            Console.WriteLine("Any key to exit dude.");
+            Console.ReadKey();
+           
+            }
+        }
+    }
---
+++ pirate.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+printf( "This hur be a pirate rrr");
+
+return 0;
+
+}
